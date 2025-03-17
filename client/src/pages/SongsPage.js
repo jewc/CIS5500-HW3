9af8cbb236240bfe9e1e72edac794e69929f7107
jewc <jamescew@gmail.com>
2025-03-17 11:33:48 -0400

    Update to client UI

--- client/src/pages/SongsPage.js
+++ client/src/pages/SongsPage.js
@@ -75,16 +75,19 @@ export default function SongsPage() {
     <Container>
       {selectedSongId && <SongCard songId={selectedSongId} handleClose={() => setSelectedSongId(null)} />}
       <h2>Search Songs</h2>
+
       <Grid container spacing={6}>
         <Grid item xs={8}>
           <TextField label='Title' value={title} onChange={(e) => setTitle(e.target.value)} style={{ width: "100%" }}/>
         </Grid>
+
         <Grid item xs={4}>
           <FormControlLabel
             label='Explicit'
             control={<Checkbox checked={explicit} onChange={(e) => setExplicit(e.target.checked)} />}
           />
         </Grid>
+
         <Grid item xs={6}>
           <p>Duration</p>
           <Slider
@@ -97,6 +100,7 @@ export default function SongsPage() {
             valueLabelFormat={value => <div>{formatDuration(value)}</div>}
           />
         </Grid>
+
         <Grid item xs={6}>
           <p>Plays (millions)</p>
           <Slider
@@ -109,8 +113,41 @@ export default function SongsPage() {
             valueLabelFormat={value => <div>{value / 1000000}</div>}
           />
         </Grid>
+
         {/* TODO (TASK 24): add sliders for danceability, energy, and valence (they should be all in the same row of the Grid) */}
         {/* Hint: consider what value xs should be to make them fit on the same row. Set max, min, and a reasonable step. Is valueLabelFormat is necessary? */}
+
+        <Grid item xs={4}>
+          <p>Danceability</p>
+          <Slider
+            value={danceability}
+            min={0}
+            max={1}
+            step={0.01}
+            onChange={(e, newValue) => setDanceability(newValue)}
+            valueLabelDisplay='auto'
+          />
+          </Grid>
+          <Grid item xs={4}>
+          <p>Energy</p>
+          <Slider
+            value={energy}
+            min={0}
+            max={1}
+            step={0.01}
+            onChange={(e, newValue) => setEnergy(newValue)}
+            valueLabelDisplay='auto' />
+            </Grid>
+            <Grid item xs={4}> 
+          <p>Valence</p>
+          <Slider
+            value={valence}
+            min={0}
+            max={1}
+            step={0.01}
+            onChange={(e, newValue) => setValence(newValue)}
+            valueLabelDisplay='auto' />
+            </Grid>
       </Grid>
       
       <Button onClick={() => search() } style={{ left: '50%', transform: 'translateX(-50%)' }}>
